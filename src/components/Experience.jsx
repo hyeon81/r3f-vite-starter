9f64e4fc6867957a093acb54cd8d8a098645b188
hyeon81 <guswly801@gmail.com>
2024-07-04 08:51:11 +0900

    add typing

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,15 @@
 import { OrbitControls } from "@react-three/drei";
+import {Avatar} from "./Avatar.jsx";
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+        <group position-y={-1}>
+        <Avatar/>
+        </group>
+        {/*없으면 검은색 됨*/}
+        <ambientLight intensity={1}/>
     </>
   );
 };
